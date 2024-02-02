1a225c0b4442847cc2b62036a203e374e4ef1ac7
Amit Rai <amitrai@amits-air.home>
2024-02-01 20:17:44 -0500

    my first commit to add to repo

--- app.py
+++ app.py
@@ -2,27 +2,37 @@
 import os
 
 import aws_cdk as cdk
+from aws_cdk import Environment
 
 from aws_cdk_lambda_canary.aws_cdk_lambda_canary_stack import AwsCdkLambdaCanaryStack
 
 
 app = cdk.App()
-AwsCdkLambdaCanaryStack(app, "AwsCdkLambdaCanaryStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+environment_type = app.node.try_get_context("environmentType")
+environment_context = app.node.try_get_context(environment_type)
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+if environment_context is not None:
+    region = environment_context["region"]
+else:
+    region = "us-east-2"
+account = app.node.try_get_context("account")
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+if environment_context is not None:
+    tags = environment_context["tags"]
+else:
+    tags={"Environment": "TEST"}
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+stack_name = f'{app.node.try_get_context("prefix")}-{environment_type}'
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+AwsCdkLambdaCanaryStack(
+        app, 
+        stack_name,
+        env = Environment(
+            account = account,
+            region = region
+        ),
+        tags=tags,
     )
 
 app.synth()
--- aws_cdk_lambda_canary/aws_cdk_lambda_canary_stack.py
+++ aws_cdk_lambda_canary/aws_cdk_lambda_canary_stack.py
@@ -1,9 +1,11 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from datetime import datetime
+from aws_cdk import Stack, RemovalPolicy
 from constructs import Construct
+from aws_cdk.aws_lambda import Function, Runtime, Code, Alias, VersionOptions
+from aws_cdk.aws_apigateway import LambdaRestApi, StageOptions
+from aws_cdk.aws_cloudwatch import Alarm, ComparisonOperator
+from aws_cdk.aws_codedeploy import LambdaDeploymentGroup, LambdaDeploymentConfig
+
 
 class AwsCdkLambdaCanaryStack(Stack):
 
@@ -12,8 +14,57 @@ class AwsCdkLambdaCanaryStack(Stack):
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkLambdaCanaryQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        environment_type = self.node.try_get_context("environmentType")
+        context = self.node.try_get_context(environment_type)
+        self.alias_name = context["lambda"]["alias"]
+        self.stage_name = context["lambda"]["stage"]
+        current_date =  datetime.today().strftime('%d-%m-%Y')
+
+        my_lambda = Function(
+            scope = self,
+            id = "MyAppFunction",
+            function_name= context["lambda"]["name"],
+            handler = "handler.lambda_handler",
+            runtime = Runtime.PYTHON_3_11,
+            code = Code.from_asset("lambda"),
+            current_version_options = VersionOptions(
+                description = f'This Version deployed on {current_date}',
+                removal_policy = RemovalPolicy.RETAIN
+            )
+        )
+
+        new_version = my_lambda.current_version
+        new_version.apply_removal_policy(RemovalPolicy.RETAIN)
+
+        alias = Alias(
+            scope = self,
+            id = "FunctionAlias",
+            alias_name = self.alias_name,
+            version = new_version
+        )
+
+        LambdaRestApi(
+            scope = self,
+            id = "RestAPI",
+            handler = alias,
+            deploy_options = StageOptions(stage_name=self.stage_name)
+        )
+
+        failure_alarm = Alarm(
+            scope = self,
+            id = "FunctionFailureAlarm",
+            metric = alias.metric_errors(),
+            threshold = 1,
+            evaluation_periods = 1,
+            alarm_description = "The latest deployment errors > 0",
+            alarm_name = f'{self.stack_name}-canary-alarm',
+            comparison_operator = ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        )
+
+        LambdaDeploymentGroup(
+            scope = self,
+            id = "CanaryDeployment",
+            alias = alias,
+            deployment_config = LambdaDeploymentConfig.CANARY_10_PERCENT_5_MINUTES,
+            alarms = [failure_alarm]
+        )
